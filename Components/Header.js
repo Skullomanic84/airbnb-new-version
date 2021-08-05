dc43248445435a09d126771abd54bedb6ffb4528
Skullomanic84 <jhshiloh@gmail.com>
2021-08-05 18:05:22 +0200

    updated header.js
    completed the navigation

--- Components/Header.js
+++ Components/Header.js
@@ -1,5 +1,9 @@
 import Image from 'next/image';
-import { SearchIcon } from '@heroicons/react/solid';
+import { SearchIcon,
+        GlobeAltIcon,
+        MenuIcon,
+        UserIcon,
+        UserCircleIcon, } from '@heroicons/react/solid';
 
 
 
@@ -7,27 +11,36 @@ function Header() {
     return (
         <navbar className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
         {/*logo section*/}
-            <div className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
-            <Image 
-            src="https://links.papareact.com/qd3"
-            layout="fill"
-            objectFit ="contain"
-            objectPosition="left" />
-            
-            </div>
+                    <div className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
+                            <Image 
+                            src="https://links.papareact.com/qd3"
+                            layout="fill"
+                            objectFit ="contain"
+                            objectPosition="left" />
+                    
+                    </div>
         {/*search section*/}
-            <div className="Navbar__search-wrapper flex item-center md:border-2 rounded-full py-2 md:shadow-sm">
+                    <div className="Navbar__search-wrapper flex item-center md:border-2 rounded-full py-2 md:shadow-sm">
 
-                <input type="text" placeholder="start your search" className="outline-none pl-5 bg-transparent flex-grow text-sm text-gray-600 placeholder-gray-400"/>
+                        <input type="text" placeholder="start your search" className="outline-none pl-5 bg-transparent flex-grow text-sm text-gray-600 placeholder-gray-400"/>>
 
-                <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
+                        <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
 
-            </div>
+                    </div>
 
-            <div className="Navbar__user-wrapper">
-            
-            </div>
+                    <div className="Navbar__user-wrapper flex items-center justify-end space-x-4 text-gray-400">
+                        
+                    <p className="hidden md-inline cursor-pointer">Become a host</p>
 
+                        <div>
+                            <GlobeAltIcon  className="h-6 cursor-pointer"/>
+
+                        </div>
+                        <div className="flex items-center space-x-2 p-2 rounded-full border-2">
+                            <MenuIcon className="h-6" />
+                            <UserCircleIcon className="h-6" />
+                        </div>
+                    </div>
         </navbar>
     )
 }
