7465af7f5df8564654313f27e1cd27ac08dc4047
Skullomanic84 <jhshiloh@gmail.com>
2021-08-05 19:08:06 +0200

    updated index.js
    Created the main title and connected the API endpoints to fetch data from our server

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Header from '../Components/Header';
 import Jambotron from '../Components/Jambotron'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className="">
       <Head>
@@ -14,7 +14,31 @@ export default function Home() {
      <Header />
      { /*Banner section */ }
      <Jambotron />
+     { /*Main section*/ }
+     <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-5">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+
+          {/*pull some data from a server - API endpoints */}
+
+          {exploreData?.map(item => (<h1>{item.location}</h1>))}
+        
+        </section>
+     
+     </main>
+
       
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const exploreData = await fetch('https://links.papareact.com/pyp').then((res) =>res.json()
+  );
+
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
