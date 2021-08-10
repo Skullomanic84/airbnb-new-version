1371799bc21ef9d2f246abf948d38d4cab6a888f
Skullomanic84 <jhshiloh@gmail.com>
2021-08-10 12:51:07 +0200

    added search.js
    created filter on the search , created tailwind global css . Added search.js page. Used Next js hook to link the home page and the search page

--- Components/Header.js
+++ Components/Header.js
@@ -2,16 +2,58 @@ import Image from 'next/image';
 import { SearchIcon,
         GlobeAltIcon,
         MenuIcon,
-        UserIcon,
+        UsersIcon,
         UserCircleIcon, } from '@heroicons/react/solid';
+
+import { useState } from 'react';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
 
 
 function Header() {
+
+    const [noOfGuests, setNoOfGuests] = useState(1) // default minmum no. of guest
+    const [searchInput, setSearchInput ] = useState('');
+    const [startDate, setStartDate] =useState(new Date());
+    const [endDate, setendDate] =useState(new Date());
+    const router = useRouter();
+
+    const selectionRange = {
+        startDate:startDate,
+        endDate:endDate,
+        key: 'selection',
+    }
+
+    const handleSelection= (ranges) => {
+        setStartDate(ranges.selection.startDate);
+        setendDate(ranges.selection.endDate);
+    };
+    // resetting the cancel button
+    const resetInput =()=> {
+        setSearchInput('');
+    };
+
+    //search filter
+    const search = () => {
+        router.push({
+            pathname:'/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),                endDate: endDate.toISOString(),
+                noOfGuests,
+
+            },
+        });
+    }
+
+
     return (
         <navbar className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
         {/*logo section*/}
-                    <div className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
+                    <div onClick={() => router.push('/')} className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
                             <Image 
                             src="https://links.papareact.com/qd3"
                             layout="fill"
@@ -22,7 +64,12 @@ function Header() {
         {/*search section*/}
                     <div className="Navbar__search-wrapper flex item-center md:border-2 rounded-full py-2 md:shadow-sm">
 
-                        <input type="text" placeholder="start your search" className="outline-none pl-5 bg-transparent flex-grow text-sm text-gray-600 placeholder-gray-400"/>
+                        <input
+                        value={searchInput}
+                        onChange={(e)=> setSearchInput(e.target.value)} 
+                        type="text" 
+                        placeholder="start your search" 
+                        className="outline-none pl-5 bg-transparent flex-grow text-sm text-gray-600 placeholder-gray-400"/>
 
                         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
 
@@ -41,8 +88,36 @@ function Header() {
                             <UserCircleIcon className="h-6" />
                         </div>
                     </div>
+
+                    {searchInput && (
+                        <div className='flex flex-col col-span-3 mx-auto'>
+                        <DateRangePicker
+                        ranges={[selectionRange]}
+                        minDate={new Date()} 
+                        rangeColors={["#fd5b61"]}
+                        onChange={handleSelection}/>
+
+                        <div className="flex items-center border-b mb-4">
+                            <h2 className='text-2xl flex-grow font-semibold'>Number of guests</h2>
+                            <UsersIcon className='h-5' />
+
+                            <input
+                            value={[noOfGuests]}
+                            onChange={(e) => setNoOfGuests(e.target.value)}
+                            min={1} 
+                            type="number" 
+                            className='w-12 pl-2text-lg outline-none text-red-400'/>
+                        </div>
+                        <div className='flex'>
+                        <button onClick ={resetInput}className='flex-grow text-gray-500'>Cancel</button>
+                        <button onClick={search} className='flex-grow text-red-400'>Search</button>
+                        </div>
+                        
+                        </div>
+                    )}
+                    
         </navbar>
-    )
+    );
 }
 
 export default Header
