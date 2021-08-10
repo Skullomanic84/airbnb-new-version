56333398e6229b0b07278d8a5321b4fe2bc24c95
Skullomanic84 <jhshiloh@gmail.com>
2021-08-10 14:34:56 +0200

    Update Header.js

--- Components/Header.js
+++ Components/Header.js
@@ -53,7 +53,8 @@ function Header({placeholder}) {
     return (
         <navbar className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
         {/*logo section*/}
-                    <div onClick={() => router.push('/')} className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
+                    <div 
+                    onClick={() => router.push("/")} className="relative Navbar__logo-wrapper flex items-center h-10 cursor-pointer my-auto">
                             <Image 
                             src="https://links.papareact.com/qd3"
                             layout="fill"
